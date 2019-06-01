f26a1df7a3fe419b2a5d9475f7d70c78b87c921b
aeisha simmons <ashicks2017@outlook.com>
2019-06-01 14:15:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See REAd
 def fizzbuzz(int)
-  if (int % 5 == 0) && (int % 3 == 0)
-    return "FizzBuzz"
-  elsif int % 3 == 0
-    return "Fizz"
-  elsif int % 5 == 0
-    return "Buzz"
-  else
-    return
-  end
+  if (int % 5 == 0)
 end
